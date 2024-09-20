c0420fd35a3e5604916c86cba7286c034f0ae8ca
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2024-09-20 15:26:44 +0500

    fix: error handling

--- src/utils/exception.util.ts
+++ src/utils/exception.util.ts
@@ -2,7 +2,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 
 export const Exception = (error: unknown) => {
   if (error instanceof Error) {
-    return error;
+    throw error;
   }
   return new InternalServerErrorException('An unknown server error occurred.');
 };
